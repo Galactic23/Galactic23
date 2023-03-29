d20ed127ab3e937ab00641a1832b6a982fa0a533
rrios119 <riosrobert23@gmail.com>
2023-03-29 15:48:57 -0500

    genre, label, release date, language and album

--- songs.js
+++ songs.js
@@ -3,8 +3,14 @@ const axios = require('axios');
 const a = require('./albums');
 
 const songs = [];
+//info = [];
 s_links = [];
 s_name = [];
+s_album = [];
+s_genre = [];
+s_label = [];
+s_release = [];
+s_language = [];
 
 async function getData1()
 {
@@ -23,6 +29,17 @@ async function getData1()
             temp = temp.replace(" Lyrics", "");
 
             s_name.push(temp);
+
+            temp_info = $('.wp-block-table td:nth-child(2)').get().map(x => $(x).text());
+            info = info.concat(temp_info);
+            
+            s_album.push(info[0]);
+            s_genre.push(info[1]);
+            s_label.push(info[2]);
+            s_release.push(info[3]);
+            s_language.push(nfo[4]);
+            
+            info = [];
         }
         catch (error)
         {
@@ -34,7 +51,7 @@ async function getData1()
 
     for (let i = 0; i < a.song_links.length; i++)
     {
-        songs.push({ Name: s_name[i], Links: s_links[i] })
+        songs.push({ Name: s_name[i], Links: s_links[i], Album: s_album[i], Genre: s_genre[i], Label: s_label[i], Release: s_release[i], Language: s_language[i] })
     }
     console.log('Songs Complete')
 }
