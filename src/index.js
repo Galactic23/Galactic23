8d321b3ab361944fd702adfe38d558ac16227a7f
rrios119 <riosrobert23@gmail.com>
2023-05-19 15:32:40 -0500

    json changes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
-const app = require('express')();
-const PORT = 8080;
-const a = require('./albums');
-const s = require('./songs');
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import './index.css'
+import App from './app'
 
-url = "https://kgasa.com/album/page/";
-songs = [];
-album = [];
-
-app.get('/', (req, res) => {
-    res.json('Welcome to Korean Lyric API')
-}) 
-
-//a.getData(url).then(data => album = data.array1); //album extraction
-//a.getData(url).then(data => album = data.array1);
-s.getData1();
-album = a.album;
-songs = s.songs;
-//songs = s.getData1();
-
-//console.log(songs)
-
-
-app.get('/album', (req, res) => 
-{
-    res.json(album)
-})
-
-app.get('/songs', (req, res) => 
-{
-    res.json(songs)
-})
-
-app.listen(
-    PORT,
-    () => console.log(`server running on http://localhost:${PORT}`)
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
