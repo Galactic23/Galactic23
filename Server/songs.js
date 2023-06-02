066f4fa3b59335da666f594179c930babf506e1b
rrios119 <riosrobert23@gmail.com>
2023-06-01 22:06:44 -0500

    bug fixes

--- Server/songs.js
+++ Server/songs.js
@@ -4,10 +4,6 @@ import { getData } from './albums.js';
 import { song_links } from './albums.js';
 import { song_album } from './albums.js';
 
-//const cheerio = require('cheerio');
-//const axios = require('axios');
-//const a = require('./albums');
-
 const songs = [];
 const hangul_kanji = /(?:[\u3131-\uD79D]+)|(?:[一-龠]+|[ぁ-ゔ]+|[ァ-ヴー]+|[々〆〤ヶ]+)/;
 let info = [];
@@ -41,22 +37,37 @@ export async function getData1(url)
 
             //let temp = ($('.entry-content h2').first().text()); //locating table with album info
             //temp = temp.replace(" Lyrics", "");
-            let temp = ($('.entry-header h1').first().text());
-            console.log(temp);
+            let temp = ($('.entry-header h1').first().html()); //.text()
+            //console.log(temp);
 
-            temp = temp.split('&nbsp;– ');
+            temp = temp.replace(/&nbsp;/g, " ");
+            //console.log(temp);
+            $('.entry-header h1').first().html(temp);
+            
+            let t = ($('.entry-header h1').first().text());
+            console.log(t);
 
-            let temp2 = temp[1];
+            t = t.split(' – ');
+
+            let temp2 = t[1]; //temp[1]
             temp2 = temp2.replace(" Lyrics", '');
 
             s_name.push(temp2);
-            s_artist.push(temp[0]);
+            s_artist.push(t[0]); //temp[0]
+
+            if (temp2.includes('Wishes') || temp2.includes('Anymore')) //using this to debug
+            {
+                //$('.entry-content h2').last().toggleClass('h3');
+                $('.entry-content h2').last().each((i, item) => (item.tagName = 'h3'))
+                console.log('Song found'); //json index 11043 song Nectar by WayV
+                //Genre, Label, Release, and Language Missing at index 14163 and forward of json
+            }
 
             //s_name.push(temp); //need to rewrite... have to get name from the title
 
             let condition = $('.wp-block-table table:nth-child(1) td:nth-child(2)').get().map(x => $(x).text());
 
-            if (condition == 6) //if table has 6 elements (Single, Album, Genre, Label, Release Date, Language)
+            if (condition.length == 6) //if table has 6 elements (Single, Album, Genre, Label, Release Date, Language)
             {
                 let temp_info = $('.wp-block-table table:nth-child(1) td:nth-child(2)').get().map(x => $(x).text());
                 //info = info.concat(temp_info);
@@ -69,7 +80,7 @@ export async function getData1(url)
                 s_release.push(temp_info[4]);
                 s_language.push(temp_info[5]);
             }
-            else //table with 5 elements (Album, Genre, Label, Release Date, Language)
+            else if (condition.length == 5) //table with 5 elements (Album, Genre, Label, Release Date, Language)
             {
                  let temp_info = $('.wp-block-table table:nth-child(1) td:nth-child(2)').get().map(x => $(x).text());
                 //info = info.concat(temp_info);
@@ -85,15 +96,16 @@ export async function getData1(url)
             //let temp_artists = ($('.entry-content h2').last().text()); //fix this get artist from title, and splice the string to only get artist name
             //s_artist.push(temp_artists);
 
+            $('.entry-content p:not([class])').after('<br>');
             $('.entry-content p:not([class])').find('br').replaceWith('\n');  //add new line when <br> exists
             let total_lyrics = $('.entry-content h3').get().map(x => $(x).text());
             let h2_size = $('.entry-content h2').get().map(x => $(x).text());
 
-            if (total_lyrics.length == 4) //total_lyrics.length == 4
+            if (total_lyrics.length == 4) //if h3 size == 4
             {
                 let head_indentity = $('.entry-content h3').first().text();
 
-                if (head_indentity.includes('ROMANIZED') || head_indentity.includes('ROMAJI') || head_indentity.includes('Romanized') || head_indentity.includes('Romaji'))
+                if (head_indentity.includes('ROMANIZED') || head_indentity.includes('ROMAJI') || head_indentity.includes('Romanized') || head_indentity.includes('Romaji') || head_indentity.includes('Pinyin'))
                 {
                     let count = 0;
                     $('.entry-content h3').get().map((e, count = 0) => $(e).addClass(count.toString()), count++); //give all h3 tags a unique class name
@@ -124,20 +136,38 @@ export async function getData1(url)
                     s_romanized.push(temp_rom);
                 }
             }
-            else if (h2_size > 2)
+            else if (h2_size.length > 2) //if h2 > 2
             {
-                let count = 0;
-                $('.entry-content h2').get().map((e, count = 0) => $(e).addClass(count.toString()), count++); //give all h3 tags a unique class name
+                let head_indentity = $('.entry-content h2').first().text();
 
-                let temp_eng = $('.0').nextUntil('.1').addBack().next('p:not([class])').text(); //lyrics for english
-                let temp_han = $('.1').nextUntil('.2').addBack().next('p:not([class])').text(); //lyrics for hangul
-                let temp_rom = $('.2').nextUntil('div').addBack().next('p:not([class])').text(); //lyrics for romanized
+                if (head_indentity.includes('ROMANIZED') || head_indentity.includes('ROMAJI') || head_indentity.includes('Romanized') || head_indentity.includes('Romaji'))
+                {
+                    let count = 0;
+                    $('.entry-content h2').get().map((e, count = 0) => $(e).addClass(count.toString()), count++); //give all h2 tags a unique class name
 
-                s_english.push(temp_eng);
-                s_hangul.push(temp_han);
-                s_romanized.push(temp_rom);    
+                    let temp_rom = $('.0').nextUntil('.1').addBack().next('p:not([class])').text(); //lyrics for romanized
+                    let temp_han = $('.1').nextUntil('.2').addBack().next('p:not([class])').text(); //lyrics for hangul
+                    let temp_eng = $('.2').nextUntil('div').addBack().next('p:not([class])').text(); //lyrics for englsih
+
+                    s_romanized.push(temp_rom);
+                    s_hangul.push(temp_han);
+                    s_english.push(temp_eng);
+                }
+                else if (head_indentity.includes('English') || head_indentity.includes('ENGLISH'))
+                {
+                    let count = 0;
+                    $('.entry-content h2').get().map((e, count = 0) => $(e).addClass(count.toString()), count++); //give all h2 tags a unique class name
+
+                    let temp_eng = $('.0').nextUntil('.1').addBack().next('p:not([class])').text(); //lyrics for english
+                    let temp_han = $('.1').nextUntil('.2').addBack().next('p:not([class])').text(); //lyrics for hangul
+                    let temp_rom = $('.2').nextUntil('div').addBack().next('p:not([class])').text(); //lyrics for romanized
+
+                    s_english.push(temp_eng);
+                    s_hangul.push(temp_han);
+                    s_romanized.push(temp_rom);
+                }
             }
-            else
+            else if (total_lyrics.length == 2 || total_lyrics.length == 3) //if h3 size == 2 
             {
                 let count = 0;
                 $('.entry-content h3').get().map((e, count = 0) => $(e).addClass(count.toString()), count++);
@@ -146,7 +176,7 @@ export async function getData1(url)
 
                 if (head_indentity.includes('ENGLISH') || head_indentity.includes('English') || head_indentity.includes('Lyrics') || head_indentity.includes('LYRICS'))
                 {
-                    let temp_eng = $('.0').nextUntil('.code-block-2').addBack().next('p:not([class])').text();
+                    let temp_eng = $('.0').nextUntil('.wp-block-table').addBack().next('p:not([class])').text();
 
                     s_english.push(temp_eng);
 
@@ -155,8 +185,8 @@ export async function getData1(url)
                 }
                 else if (head_indentity.includes('HANGUL') || head_indentity.includes('歌詞'))
                 {
-                    let temp_han = $('.0').nextUntil('.code-block-2').addBack().next('p:not([class])').text();
-
+                    let temp_han = $('.0').nextUntil('.wp-block-table').addBack().next('p:not([class])').text();
+                    //previously code-block-2
                     s_hangul.push(temp_han);
 
                     s_english.push('Unavailable');
@@ -172,10 +202,6 @@ export async function getData1(url)
             console.log(error);
         }
     }
-
-    //change name to h1
-    //at index 5739 of json file, lyrics get scrambled (Monsta X - RUSH HOUR) 
-    //error occurs since title heading for lyric order is from English, Hangul and Romanized instead of Romanized, Hangul and English
     //1255 songs extracted out of 1268
     //1235
 
@@ -188,5 +214,3 @@ export async function getData1(url)
     //insertData();
 }
 export { songs };
-//module.exports = { getData1 };
-//module.exports = { getData1, songs};
