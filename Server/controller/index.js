3f8f04eb78a844e8f75804563f059c90f9df6826
rrios119 <riosrobert23@gmail.com>
2023-06-14 17:46:36 -0500

    implementing routes

--- Server/controller/index.js
+++ Server/controller/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { getData1, songs } from './songs.js';
 import { album } from './albums.js';
+import albumRouter from '../routes/album-route.js';
+import songRouter from '../routes/song-route.js';
 
 const app = express();
 const PORT = 8080;
@@ -9,26 +11,31 @@ let url = "https://kgasa.com/album/page/";
 let songs_arr = [];
 let album_arr = [];
 
-app.get('/', (req, res) => {
-    res.json('Welcome to Korean Lyric API')
-}) 
-
 getData1(url);
 album_arr = album;
 songs_arr = songs;
 
-app.get('/album', (req, res) => 
-{
-    res.json(album);
+app.get('/', (req, res) => {
+    res.json('Welcome to Korean Lyric API')
 });
 
-app.get('/songs', (req, res) => 
-{
-    res.json(songs)
-})
+app.use('/api', albumRouter);
+
+app.use('/api', songRouter);
 
 app.listen(
     PORT,
     () => console.log(`server running on http://localhost:${PORT}`)
 );
+
+//app.get('/album', (req, res) => 
+//{
+//    res.json(album);
+//});
+
+//app.get('/songs', (req, res) => 
+//{
+//   res.json(songs)
+//})
+
 export { album_arr, songs_arr };
