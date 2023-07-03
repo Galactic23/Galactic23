b7863e5083e2d4af838afb068cbaccaa526fc5da
rrios119 <riosrobert23@gmail.com>
2023-07-03 02:56:24 -0500

    Implementing Chakra UI

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import theme from './theme.js';
+import { ThemeProvider, CSSReset, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Routes>
+            <Route 
+              path="/"
+              element={
+                <HomePage />
+              }
+            />
+
+
+          </Routes>
+      </ChakraProvider>
+    </ThemeProvider>
+  </BrowserRouter>
+);
