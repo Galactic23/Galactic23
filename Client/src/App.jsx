9793d8cfbfb370173665a53bf9d0c30c01bb5d7a
rrios119 <riosrobert23@gmail.com>
2023-07-06 02:45:50 -0500

    removed chakraui

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
 import HomePage from './pages/HomePage';
 import NavBar from './components/common/Navbar';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react';
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Flex ml={{ base: '0rem', lg: '8rem' }}
-      mt={{ base: '4rem', lg: '0rem' }}
-      justifyContent={"center"}>
+      <div className="ml-8 mt-4 lg:ml-0 lg:mt-0 flex justify-center">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <HomePage />
-            }
-          />
-
+          <Route path="/" element={<HomePage />} />
         </Routes>
-      </Flex>
+      </div>
     </BrowserRouter>
   )
 };
 
-export default App
+export default App;
