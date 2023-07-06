eab02094559656d0991e495310c5fe50312007db
rrios119 <riosrobert23@gmail.com>
2023-07-06 02:47:52 -0500

    added new get request for recently added albums

--- Server/routes/album-route.js
+++ Server/routes/album-route.js
@@ -2,12 +2,13 @@ import Router from 'express';
 import supabase from '../config/supabaseClient.js';
 
 const albumRouter = Router();
+const newAlbums = Router();
 
 albumRouter.get('/albums', async (req, res) => {
     try {
-        const {data: albums, error } = await supabase.from('albums').select('*').order('id', { ascending: true });
-        if (error) {
-            throw error;
+        const {data: albums, error: albumsError } = await supabase.from('albums').select('*').order('id', { ascending: true });
+        if (albumsError) {
+            throw albumsError;
         }
         res.json(albums);
     } catch (error) {
@@ -16,4 +17,17 @@ albumRouter.get('/albums', async (req, res) => {
     }
 });
 
-export default albumRouter;
+newAlbums.get('/recent_albums', async (req, res) => {
+    try {
+        const {data: recentAlbums, error: recentAlbumsError} = await supabase.from('albums').select('*').order('id', { ascending: false }).limit(15);
+        if (recentAlbumsError) {
+            throw recentAlbumsError;
+        }
+        res.json(recentAlbums);
+    } catch (error) {
+        console.error('Error retrieving data from supabase:', error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+});
+
+export { albumRouter, newAlbums };
