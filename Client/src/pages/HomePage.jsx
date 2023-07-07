8293af3642207c8ca0892314701d70fc6e49235a
rrios119 <riosrobert23@gmail.com>
2023-07-07 02:05:58 -0500

    Updated HomePage.jsx

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useFetchNewAlbums from '../../APIs/useNewAlbums';
 import useFetchNewSongs from '../../APIs/useNewSongs';
 import AlbumSlider from '../components/Home/albumSlider';
+import SongSlider from '../components/Home/songSlider';
 
 const HomePage = () => {
     const { albums, errorAlbum, loadingAlbum} = useFetchNewAlbums();
@@ -21,7 +22,15 @@ const HomePage = () => {
     }
 
     return (
-        <AlbumSlider albums={albums} />
+        <>
+            <div className='flex flex-row justify-center lg:max-w-full h-[40%]'>
+                
+            </div>
+            <div className="grid grid-rows-auto px-[1%] gap-10" style={{ marginTop: '-3rem' }}>
+                <AlbumSlider albums={albums} />
+                <SongSlider songs={songs} />
+            </div>
+        </>
     );
 };
 
