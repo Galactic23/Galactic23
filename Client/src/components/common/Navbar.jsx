5823c3e0dae18d7c9fc00d4cd9340d122aaf6cb0
rrios119 <riosrobert23@gmail.com>
2023-07-07 02:05:18 -0500

    Updated Navbar.jsx

--- Client/src/components/common/Navbar.jsx
+++ Client/src/components/common/Navbar.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div className="flex flex-row lg:flex-col bg-red w-full h-5rem lg:h-screen">
-            <div className="bg-blue flex p-4 h-full">
+        <div className='flex flex-col bg-white-100 px-4'>
+            <div className="bg-white-200 flex p-4">
                 <Link to="/" className="hover:no-underline">
                     <div className="flex h-4.5rem w-11.5rem rounded bg-white items-center justify-center">
                         <img
                             src="src/assets/finger-heart.png"
                             alt="Logo"
-                            className="w-16 h-16 mr-2"
+                            className="w-16 h-16"
                         />
-                        <h1 className="font-extrabold text-4xl text-purple-400">Myuzige</h1>
+                        <h1 className="font-extrabold text-4xl text-purple-400 pr-2">Myuzige</h1>
                     </div>
                 </Link>
             </div>
-    </div>
+        </div>
     )
 };
 
