9221b7e6cb26070aba980a67e837b044ff216e06
rrios119 <riosrobert23@gmail.com>
2023-07-07 01:59:36 -0500

    Updated screen sizes

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '720px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '1080px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '2560px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 }
