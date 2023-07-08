c8d3c3892e660d4d154b8e3c79898e533cd60743
rrios119 <riosrobert23@gmail.com>
2023-07-07 21:50:03 -0500

    get recent ost albums

--- Server/routes/album-route.js
+++ Server/routes/album-route.js
@@ -3,6 +3,7 @@ import supabase from '../config/supabaseClient.js';
 
 const albumRouter = Router();
 const newAlbums = Router();
+const newAlbumsOST = Router();
 
 albumRouter.get('/albums', async (req, res) => {
     try {
@@ -12,7 +13,7 @@ albumRouter.get('/albums', async (req, res) => {
         }
         res.json(albums);
     } catch (error) {
-        console.error('Error retrieving data from supabase:', error);
+        console.error('Error retrieving albums from supabase:', error);
         res.status(500).json({ error: 'Internal Server Error'});
     }
 });
@@ -25,9 +26,22 @@ newAlbums.get('/recent_albums', async (req, res) => {
         }
         res.json(recentAlbums);
     } catch (error) {
-        console.error('Error retrieving data from supabase:', error);
+        console.error('Error retrieving recent albums from supabase:', error);
         res.status(500).json({ error: 'Internal Server Error'});
     }
 });
 
-export { albumRouter, newAlbums };
+newAlbumsOST.get('/recent_ost_albums', async (req, res) => {
+    try {
+        const {data: recentAlbumsOST, error: recentAlbumsOSTError} = await supabase.from('albums').select('*').order('genre', { ascending: false }).order('release', { ascending: false }).limit(15);
+        if (recentAlbumsOSTError) {
+            throw recentAlbumsOSTError;
+        }
+        res.json(recentAlbumsOST);
+    } catch (error) {
+        console.error('Error retrieving recent OST albums from supabase:', error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+})
+
+export { albumRouter, newAlbums, newAlbumsOST };
