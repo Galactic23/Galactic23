5e5b45057c4f022dcaf03395d4597bf7f6017a4a
rrios119 <riosrobert23@gmail.com>
2023-07-07 21:55:05 -0500

    Updated albumSlider.jsx

--- Client/src/components/Home/albumSlider.jsx
+++ Client/src/components/Home/albumSlider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Flex, Box, Grid, Text, Image, IconButton, Tooltip } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
-import { Sparkle } from 'lucide-react';
+import { Sparkle, DotIcon } from 'lucide-react';
 
 const AlbumSlider = ({ albums }) => {
   const scrollContainerRef = React.useRef(null);
@@ -26,12 +26,12 @@ const AlbumSlider = ({ albums }) => {
 
   return (
     
-    <div className="flex flex-col gap-4 rounded-lg bg-gray-200 h-auto lg:max-w-full overflow-x-auto">
+    <div className="flex flex-col gap-4 rounded-lg bg-gray-100 h-auto overflow-x-auto">
       <div className='flex ml-[2.5%] justify-left'>
-        <Sparkle size={'35px'} color='purple'/>
-        <h2 className='px-4 font-mono font-bold text-3xl'>New Album Releases</h2>
+        <Sparkle size={'35px'} color='blue'/>
+        <h2 className='px-4 font-mono font-semibold text-[25px]'>Recent Album Releases</h2>
       </div>
-      <div className="flex flex-row gap-4 items-center rounded-lg bg-gray-200 h-auto lg:max-w-full overflow-x-auto">
+      <div className="flex flex-row gap-4 items-center rounded-lg bg-gray-200 h-auto relative" >
         <button
           className="ml-2 p-2 rounded-full hover:bg-gray-200"
           onClick={handleScrollLeft}
@@ -42,18 +42,20 @@ const AlbumSlider = ({ albums }) => {
           {albums.map((album) => (
             <div
               key={album.id}
-              className="flex flex-col bg-gray-200 p-4 rounded-md shadow-md truncate border border-gray-300 shadow-lg"
-              style={{ minWidth: '250px' }}
+              className="flex flex-col bg-gray-100 p-4 rounded-md truncate border border-gray-300 shadow-lg"
+              style={{ minWidth: '225px' }}
             >
               <div className="w-48 h-48 rounded-lg flex items-center justify-center mx-auto">
                 <img src={album.cover} alt="Album Cover" className="rounded-lg" />
               </div>
-              <div className="text-center">
-                <div className="w-full overflow-hidden text-black text-lg font-semibold">
+              <div className="text-center justify-center items-center">
+                <div className="w-full overflow-hidden text-black text-lg font-mono font-semibold">
                   {album.name}
                 </div>
-                <div className="w-full overflow-hidden text-black text-sm">{album.artist}</div>
-                <div className="text-black">{album.genre}</div>
+                <div className="w-full overflow-hidden text-gray-500 text-base font-serif font-normal">{album.artist}</div>
+                <div className='flex flex-row justify-center'> 
+                  <div className="text-gray-500 font-serif text-sm font-normal flex flex-row items-center">{album.genre} <DotIcon /> {album.release}</div>
+                </div>
               </div>
             </div>
           ))}
