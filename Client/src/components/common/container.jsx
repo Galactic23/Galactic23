f905f000721b0e229ca1bb398c1f8ff49597bafc
rrios119 <riosrobert23@gmail.com>
2023-07-10 00:16:33 -0500

    Updated container.jsx

--- Client/src/components/common/container.jsx
+++ Client/src/components/common/container.jsx
@@ -5,14 +5,12 @@ const Container = (props) => {
   const bg = 'bg-white dark:gray.800';
   return (
     <>
-      <div className='flex'>
-        <header className='flex flex-col w-[72] bg-white-100 min-h-screen border border-gray-300 fixed left-0 px-4'>
-          <NavBar />
-        </header>
-        <div className="bg-gray-100 w-screen justify-center ml-72 overflow-y-auto">
-          <div className="flex-1 w-full min-h-screen lg:mt-0 justify-center max-w-8xl">
-            {props.children}
-          </div>
+      <header className='w-full bg-white h-24 fixed bottom-0 top-0 left-0'>
+        <NavBar />
+      </header>
+      <div className="bg-gray-100 w-screen justify-center overflow-y-auto">
+        <div className="flex-1 w-full min-h-screen lg:mt-0 justify-center max-w-8xl">
+          {props.children}
         </div>
       </div>
     </>
